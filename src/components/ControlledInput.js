7427eb40a7d24bc0c9ce863359657668dd11b9c0
github username <github email address>
2018-08-30 17:12:02 -0400

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,22 @@
 // Code ControlledInput Component Here
+
+import React from 'react'
+
+export default class ControlledInput extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      firstName: "Joe"
+    }
+  }
+
+  handleFirstNameChange = (event) => (
+    this.setState({
+      firstName: event.target.value
+    })
+  )
+
+  render() {
+    return (<input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />)
+  }
+}
